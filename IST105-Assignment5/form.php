7b4ca3a2be0c1d74f092651b9890584a5dac8f2e
natnlg8 <natnlg8@gmail.com>
2025-02-11 21:54:21 -0800

    Update form.php

---
+++ IST105-Assignment5/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Treasure Hunt</title>
+</head>
+<body>
+    <h2>Welcome to the Interactive Treasure Hunt!</h2>
+    <form action="process.php" method="post">
+        Number (e.g., birth year): <input type="text" name="user_number" required><br>
+        Text (e.g., name or secret word): <input type="text" name="user_text" required><br>
+        <input type="submit" value="Solve the Puzzle">
+    </form>
+</body>
+</html>
